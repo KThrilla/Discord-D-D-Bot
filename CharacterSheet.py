699d7c2293aac6aa940c8dd2702a3feadffb7edd
Kevin D <kevinjosediaz15@gmail.com>
2021-10-31 05:47:52 +0000

    Initial commit

---
+++ CharacterSheet.py
@@ -0,0 +1,24 @@
+class CharacterSheet:
+  def __init__(self, attack, defense, luck, items):
+    self.attack = attack
+    self.defense = defense
+    self.luck = luck
+    self.items = items
+  
+  def sheet(self, givenRace, givenClass):
+    if givenRace == "human":
+      attack = 5
+      defense = 5
+      luck = 5
+    
+    if givenClass == "knight":
+      items = ["sword", "shield"]
+    
+    characterSheet = {
+    "Attack": attack,
+    "Defense": defense,
+    "Luck": luck,
+    "Items": items
+    }
+
+    return characterSheet
---
+++ main.py
@@ -0,0 +1,113 @@
+import discord
+import os
+import time
+from CharacterSheet import CharacterSheet
+#from keep_alive import keep_alive
+
+client = discord.Client()
+
+playerList = []
+runGame = True
+dungeonMaster = ""
+
+@client.event
+async def on_ready():
+  print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+  #Start of Message Handler
+  if message.author == client.user:
+    return
+    
+####### Dungeon Master Methods ##############################
+  async def helpDungeonMaster():
+    await message.channel.send("Dungeon Master Commands:\n$help - look up commands\n$add player - add a player mid-game\n$delete player - remove a player from the game\n$skip - skip a player's turn\n$event - allows players to do what they want\n$fight # [name of enemy] - make as many enemies fight the players\n$end - ends the game")
+  
+  async def addPlayers():
+    msg = await client.wait_for('message')
+
+    for player in msg.content.split(" "):
+      playerList.append(player)
+
+  async def deletePlayers():
+    msg = await client.wait_for('message')
+
+    for player in msg.content.split(" "):
+      playerList.remove(player)
+  
+  async def skipPlayer():
+    return #TODO
+  
+  async def eventTurn():
+    return #TODO
+
+  async def endGame():
+    await message.channel.send("Dungeon Master Ended The Game")
+    quit()
+
+
+  ####### Player Methods ####################################
+  async def helpPlayer():
+    await message.channel.send("Dungeon Master Commands:\n$help - look up commands\n$attack - attacks enemy\n$defend - Reduces incoming damage\n$run - run away from a fight\n$sheet - look at your character sheet")
+
+  async def attack():
+    return #TODO
+
+  async def defend():
+    return #TODO
+
+  async def run():
+    return #TODO
+
+  async def characterSheet(givenRace, givenClass):
+    sheet = CharacterSheet.sheet(givenRace, givenClass)
+    print(sheet)
+  
+
+####### Start Of The Game ##############################
+  if ('<@!904149165318238261>' in message.content or '<@904149165318238261>' in message.content) and message.channel.name == 'bot-stuff' and len(playerList) == 0:
+    dungeonMaster = message.author.name
+    await message.channel.send(str(dungeonMaster) + "is Dungeon Master \nMention the Players")
+
+    time.sleep(1)
+    await addPlayers()
+    await message.channel.send("Players: " + str(playerList))
+
+####### Run Game Loop Start ##############################
+  while runGame:
+    msg = await client.wait_for('message')
+
+    ########## If Statements for Dungeon Master ############################
+    if msg.author.name == dungeonMaster and msg.content.startswith("$help"):
+      await helpDungeonMaster()
+      
+    if msg.author.name == dungeonMaster and msg.content.startswith("$add player"):
+      await addPlayers()
+      await message.channel.send("Players: " + str(playerList))
+    
+    if msg.author.name == dungeonMaster and msg.content.startswith("$delete player"):
+      await deletePlayers()
+      await message.channel.send("Players: " + str(playerList))
+    
+    if msg.author.name == dungeonMaster and msg.content.startswith("$skip"):
+      await skipPlayer()
+    
+    if msg.author.name == dungeonMaster and msg.content.startswith("$event"):
+      await eventTurn()
+
+    if msg.author.name == dungeonMaster and msg.content.startswith("$end"):
+      await endGame()
+    
+    ########## If Statements for Dungeon Master ############################
+    if msg.author.name == dungeonMaster and msg.content.startswith("$end"):
+      await endGame()
+    
+
+    #while(runGame):
+      
+
+
+
+#keep_alive()
+client.run(os.environ['BotToken'])
